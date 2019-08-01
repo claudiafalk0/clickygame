bd7941c6d9cff0bb81f115146bf67145db6dc22c
Claudia Falk <claudiafalk0@gmail.com>
2019-07-31 20:38:46 -0600

    build

---
+++ build/precache-manifest.7939767e3b0d995e5add297b23be5b5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fdce37020db8442d3a8b5e42fe0a68",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "adb01ffbcd56caf16b13",
+    "url": "/clickygame/static/css/main.4b3d50eb.chunk.css"
+  },
+  {
+    "revision": "0934f8ee34c9af8654a2",
+    "url": "/clickygame/static/js/2.2f2cec0d.chunk.js"
+  },
+  {
+    "revision": "adb01ffbcd56caf16b13",
+    "url": "/clickygame/static/js/main.f587ec50.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
