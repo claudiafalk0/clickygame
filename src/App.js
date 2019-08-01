c6ec5ed9a8588b3d7a4c07838918c74c347a144e
Claudia Falk <claudiafalk0@gmail.com>
2019-07-31 20:12:03 -0600

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,80 +1,83 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ImagesCard from "./imagesCard";
 import Wrapper from "./Wrapper";
 import images from './images.json';
 import "./App.css";
 // import { runInThisContext } from "vm";
 import update from 'immutability-helper';
+
 const shuffle = require('shuffle-array');
 shuffle(images);
 
-const InitialArr = { images };
+const InitialArr = {images};
 
 class App extends Component {
-  state = {
-    images,
-    count: 0
-  };
-  // constructor(props) {
-  //   super(props);
-  //   this.state = InitialArr
-  //   let count = 0;
-  // }
+    state = {
+        images,
+        count: 0
+    };
+    // constructor(props) {
+    //   super(props);
+    //   this.state = InitialArr
+    //   let count = 0;
+    // }
 
-  restart = () => {
-    alert(`I'm sorry but you've already chosen that image`);
-    const temp = this.state.images.map(item => item.click = false);
+    restart = () => {
+        alert(`I'm sorry but you've already chosen that image`);
+        const temp = this.state.images.map(item => item.click = false);
 
-    this.setState({temp});
+        this.setState({temp});
 
-  };
+    };
 
-  changeCount = () => {
-    this.setState({count: this.state.count + 1})
-  };
+    changeCount = () => {
+        this.setState({count: this.state.count + 1})
+    };
 
-  changeClick = (id) => {
+    changeClick = (id) => {
 
-      let flag = false, clicker = this.state.images.map(
-          function (item) {
-              if (item.id === id && !item.click) {
-                  item.click = true;
+        let flag = false, clicker = this.state.images.map(
+            function (item) {
+                if (item.id === id && !item.click) {
+                    item.click = true;
                 } else if (item.id === id && item.click === true) {
-                  flag = true;
+                    flag = true;
                 }
                 return true;
-              });
-              if (flag) {
-                this.restart()
-                this.setState({count: 0})
-                
-              } else {
-                this.setState({clicker});
-                this.changeCount()
-      }
-      shuffle(images);
+            });
+        if (flag) {
+            this.restart()
+            this.setState({count: 0})
 
-  };
+        } else {
+            this.setState({clicker});
+            this.changeCount()
+        }
+        shuffle(images);
 
-  render() {
-    return (
-      <Wrapper>
-        <h1 className='title'>Click an image to begin</h1>
-        <div>
-          Score: {this.state.count}
-        </div>
-        {this.state.images.map(item => (
-          <ImagesCard
-            id={item.id}
-            key={item.id}
-            image={item.image}
-            changeClick={this.changeClick}
-            click={item.click}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+    };
+
+    render() {
+        return (
+            <BrowserRouter basename="/course-directory">
+                <Wrapper>
+                    <h1 className='title'>Click an image to begin</h1>
+                    <div>
+                        Score: {this.state.count}
+                    </div>
+                    {this.state.images.map(item => (
+                        <ImagesCard
+                            id={item.id}
+                            key={item.id}
+                            image={item.image}
+                            changeClick={this.changeClick}
+                            click={item.click}
+                        />
+                    ))}
+                </Wrapper>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
